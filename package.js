f8c4c99bf853a88438e5a23100e100c48927bc03
Nathan Muir <ndmuir@gmail.com>
2015-01-28 22:18:00 +1000

    Fix package requirements

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: '3stack:presence',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'Track who\' online on your app, and what they\'re up to',
   git: 'https://github.com/3stack-software/meteor-presence',
   documentation: 'README.md'
@@ -9,15 +9,23 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.2');
 
-  api.export('Presence');
-  api.export('presences');
+  api.export([
+    'Presence',
+    'presences'
+  ]);
 
+  api.use([
+    'coffeescript',
+    'underscore',
+    'check',
+    'mongo',
+    'ddp',
+    'accounts-base'
+  ]);
   api.use('tracker', 'client');
-  api.use('random', 'server');
-  api.use('mongo');
-
-  api.use('coffeescript');
-  api.use('accounts-base');
+  api.use([
+    'random'
+  ], 'server');
 
   api.addFiles(['lib/collection.coffee', 'lib/heartbeat.coffee']);
   api.addFiles(['lib/client/monitor.coffee', 'lib/client/presence.coffee'], 'client');
