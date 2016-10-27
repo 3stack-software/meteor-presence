e281812c797bb09b828b3b381dd0fc1d511ad872
Nathan Muir <ndmuir@gmail.com>
2016-10-27 10:23:00 +1000

    Release 1.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: '3stack:presence',
-  version: '1.0.4',
+  version: '1.0.5',
   summary: 'Track who\' online on your app, and what they\'re up to',
   git: 'https://github.com/3stack-software/meteor-presence',
   documentation: 'README.md'
